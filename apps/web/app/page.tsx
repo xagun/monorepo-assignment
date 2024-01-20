9e3bab85302e8345c9dc421038105c404eea36c1
xagun <xagun.shrestha@gmail.com>
2024-01-20 12:36:13 +0545

    tailwind added in project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,18 @@
 
 
 
+import {Button} from "assignment-ui"
+
 export default function Page(): JSX.Element {
   return (
     <main>
-     Hello
+    <div className="bg-red-600">
+    <p className="p-4">
+    Hello
+    </p>
+
+<Button />
+    </div>
     </main>
   );
 }
